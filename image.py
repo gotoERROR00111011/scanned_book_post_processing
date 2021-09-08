84008aae8b92b5908bd7a411c966867d102d2541
unknown <gotoerror00111011@gmail.com>
2021-09-08 23:57:26 +0900

    add image to low png

--- image.py
+++ image.py
@@ -3,32 +3,6 @@ import cv2
 import numpy as np
 
 
-def get_ratio(img):
-    h, w = (img.shape[0], img.shape[1])
-    w = w/h
-    h = 1
-    return h, w
-
-
-def remove_background(img, thresh=220):
-    if len(img.shape) == 2:
-        _, threshold = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
-    else:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, threshold = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
-        threshold = cv2.cvtColor(threshold, cv2.COLOR_GRAY2BGR)
-    img = cv2.add(img, threshold)
-    return img
-
-
-def histogram_compress(img):
-    #img = np.array(img/5, dtype=np.int) * 5
-    img = np.array(img/25, dtype=np.int) * 25
-    img[img > 235] = 255
-    img = np.array(img, dtype=np.float32)
-    return img
-
-
 def resize(img, height=1200):
     h, w = get_ratio(img)
     h = int(height * h)
@@ -44,16 +18,35 @@ def imwrite(trg_path, img):
         encoded_img.tofile(f)
 
 
+def get_ratio(img):
+    h, w = (img.shape[0], img.shape[1])
+    w = w/h
+    h = 1
+    return h, w
+
+
+def histogram_compress(img):
+    img = np.array(img/25, dtype=np.int) * 25
+    img[img > 235] = 255
+    img = np.array(img, dtype=np.float32)
+    return img
+
+
+def remove_background(img, thresh=150):
+    #img = resize(img)
+    h = int(img.shape[0]/12)
+    w = int(img.shape[1]/8)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(h, w))
+    equalized = clahe.apply(img)
+    # equalized = cv2.equalizeHist(img)
+
+    _, threshold = cv2.threshold(equalized, thresh, 255, cv2.THRESH_BINARY)
+    img = cv2.add(img, threshold)
+    return img
+
+
 def convert_page(src_path, mode):
     ff = np.fromfile(src_path, np.uint8)
     img = cv2.imdecode(ff, mode)
-    #img = remove_background(img)
+    img = remove_background(img)
     return resize(img)
-
-
-def remove_finger(img):
-    h, w = (img.shape[0], img.shape[1])
-    top, bottom = (0, h)
-    left, right = (0, w)
-    img = img[:, left + 30: right - 30]
-    return img
--- pdf2img.py
+++ pdf2img.py
@@ -1,6 +1,6 @@
 from pdf import *
 from path import *
-# from image import *
+from image import *
 
 set_default_dirs()
 
@@ -11,9 +11,18 @@ trg_pdf_path = '03_trg_pdf'
 
 # pdfs to images
 for pdf in get_all_paths(src_pdf_path, src_img_path, "*.pdf"):
+    break
     trg_dir = pdf.replace(src_pdf_path, src_img_path)
     trg_dir = trg_dir.replace(".pdf", "")
     mkdir(trg_dir)
     pdf_to_images(pdf, trg_dir)
 
 # images to gray
+images = get_all_paths(src_img_path, trg_img_path, "*.jpg")
+
+for image in images:
+    trg = image.replace(src_img_path, trg_img_path)
+    trg = trg.replace("jpg", "png")
+    img = convert_page(image, cv2.IMREAD_GRAYSCALE)
+    img = histogram_compress(img)
+    imwrite(trg, img)
